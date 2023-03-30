80783f075f6c9befb2ee7f766c55e9b1472105aa
Hagiber <hagiber@hotmail.com>
2023-03-30 18:24:45 +0200

    Start...

---
+++ android/src/com/bb2/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bb2.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bb2.game.BombshellBoxerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BombshellBoxerGame(), config);
+	}
+}
